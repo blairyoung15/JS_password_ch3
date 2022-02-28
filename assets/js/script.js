b9ae485295b6df91a4e3c76774579a5892857426
blairyoung15 <bey99d@gmail.com>
2022-02-27 19:37:09 -0600

    added character length variable with function

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,14 @@ function writePassword() {
 }
 
 // function to generate password (var password = generatePassword)
-
+function generatePassword() {
 //  1. prompt user for password criteria
 //    a. password length 8-128 characters.
+var length = prompt("Choose the length of your password (between 8-128 characters)", 8);
+    if (length < 8 || length > 128 || isNaN(length) === true) {
+        alert("Please enter a length between 8-128 characters");
+        return;
+  }
 //    b. lowercase, uppercase, numbers, special characters
 //  2. confirm choices for characters criteria 
 //  3. generate password based on criteria 
