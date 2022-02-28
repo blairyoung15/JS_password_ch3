37b532699b683428290dd094cbeb2d49f953ac35
blairyoung15 <bey99d@gmail.com>
2022-02-27 19:31:09 -0600

    pseudocoding for what is needed to create successful generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,4 @@
 // Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -13,5 +11,16 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// function to generate password (var password = generatePassword)
+
+//  1. prompt user for password criteria
+//    a. password length 8-128 characters.
+//    b. lowercase, uppercase, numbers, special characters
+//  2. confirm choices for characters criteria 
+//  3. generate password based on criteria 
+//  4. display password on page
+
+
+
+// Add event listener to generate button (keep at bottom of code)
 generateBtn.addEventListener("click", writePassword);
