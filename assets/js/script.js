8735b532d6c7536b46ca111ee594a20b60e233a7
blairyoung15 <bey99d@gmail.com>
2022-02-27 19:38:15 -0600

    added variable arrays for the different types of characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,14 @@ var length = prompt("Choose the length of your password (between 8-128 character
         return;
   }
 //    b. lowercase, uppercase, numbers, special characters
+
+var lowerChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var numberChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(", ")', '+', '-', '.', '`', '~', '|', '<', '>', '=', '-', '_'];
+var characters = [];
+
+
 //  2. confirm choices for characters criteria 
 //  3. generate password based on criteria 
 //  4. display password on page
